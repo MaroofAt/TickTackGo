5bb42942f0c82b54531a9bcd69c7be3d5b6c48de
ali almsfi <AliAlmusfi>
2025-05-21 12:09:19 +0300

    add UserViewSet in view.py | add UserSerializer in serializer.py

--- backend/src/users/views.py
+++ backend/src/users/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .models import User
+from .serializers import UserSerializer , RegisterSerializer
+from rest_framework import status 
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        if not self.request.user.is_staff:
+            qs = qs.filter(username = self.request.user.username)
+
+        return qs
+
+    @action(detail=False , methods=['post'] , serializer_class=RegisterSerializer):
+    def register(self , request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status = status.HTTP_201_CREATED)
+        return Response(serializer.data , status = status.HTTP_400_BAD_REQUEST)
