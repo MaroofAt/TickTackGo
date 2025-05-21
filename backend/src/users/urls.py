f6e8cdcec9f289504b753346d00883e30a25e091
ali almsfi <AliAlmusfi>
2025-05-21 13:37:52 +0300

    Modified RegisterSerializer (Add the Questions) | test the Register API

--- backend/src/users/urls.py
+++ backend/src/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from views import UserViewSet
+from .views import UserViewSet
 
 router_user = DefaultRouter()
-router_user.register(r'user' , UserViewSet)
+router_user.register(r'users' , UserViewSet)
 
 urlpatterns = [
     *router_user.urls
