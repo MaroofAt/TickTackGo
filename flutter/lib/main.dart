58137b337000f01480750591aebc78c860e489bc
unknown <shadishahda23@gmail.com>
2025-05-23 08:28:54 +0300

    push test

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -10,12 +10,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme(),
-        routes: routes,
-      home:Container() );
+      routes: routes,
+      home: Container(),
+    );
   }
 }
