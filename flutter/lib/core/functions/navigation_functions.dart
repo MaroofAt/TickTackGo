d09d4cd9279bd3a70857d53bca2975cc14b9449c
gharam othman <gharam200410@gmail.com>
2025-05-23 03:24:30 +0300

    push the project

---
+++ flutter/lib/core/functions/navigation_functions.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+pushScreen(BuildContext context, Widget secondScreen) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => secondScreen),
+  );
+}
+
+pushReplacementScreen(BuildContext context, Widget secondScreen) {
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(builder: (context) => secondScreen),
+  );
+}
+
+pushReplacementNamed(BuildContext context, String route) {
+  Navigator.of(context).pushReplacementNamed(route);
+}
+
+pushNamed(BuildContext context, String route) {
+  Navigator.pushNamed(context, route);
+}
+
+pushNamedAndRemoveUntil(BuildContext context, String route) {
+  Navigator.pushNamedAndRemoveUntil(
+    context,
+    route,
+    (route) => false,
+  );
+}
+
+popScreen(BuildContext context) {
+  Navigator.pop(context);
+}
+
+popUntil(BuildContext context,String routes) {
+  Navigator.popUntil(context, ModalRoute.withName(routes));
+}
