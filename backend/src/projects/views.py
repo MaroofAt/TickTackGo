8b1b9bf6139b2c5cd6dbad44fb4131aed6098b8c
MaroofAt <maroof95at@gmail.com>
2025-06-03 13:01:14 +0300

    Created ProjectSerializer and ProjectMembershipSerializer

--- backend/src/projects/views.py
+++ backend/src/projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Project
 
 # Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = None #TODO
+    permission_classes = [IsAuthenticated]
+    
