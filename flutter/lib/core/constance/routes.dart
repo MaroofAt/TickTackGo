141cfbed2cd10c9911a8b7d604eba62ee9d51592
gharam othman <gharam200410@gmail.com>
2025-06-13 13:45:50 +0300

    new ui for signup and signin, ui for creat project and start with the show all projects

--- flutter/lib/core/constance/routes.dart
+++ flutter/lib/core/constance/routes.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:pr1/business_logic/intro_questions_cubit/intro_questions_cubit.dart';
 import 'package:pr1/core/constance/strings.dart';
 import 'package:pr1/presentation/screen/auth/signin.dart';
+import 'package:pr1/presentation/screen/auth/signinnew.dart';
 import 'package:pr1/presentation/screen/auth/signup.dart';
+import 'package:pr1/presentation/screen/auth/signupnew.dart';
 import 'package:pr1/presentation/screen/intro_questions/intro_questions_main.dart';
 import 'package:pr1/presentation/screen/onboarding/final_onboarding_page.dart';
 import 'package:pr1/presentation/screen/onboarding/onboarding_main.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  signupRoute: (context) => SignUp(),
-  signinRoute: (context) => SignIn(),
+  signupRoute: (context) => Signupnew(),
+  signinRoute: (context) => Signinnew(),
   onboardingMainRoute: (context) => const OnboardingMain(),
   introQuestionsRoute: (context) =>
       BlocProvider(
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:pr1/core/constance/routes.dart';
+import 'package:pr1/presentation/screen/auth/signin.dart';
+import 'package:pr1/presentation/screen/auth/signinnew.dart';
+import 'package:pr1/presentation/screen/auth/signupnew.dart';
 import 'package:pr1/presentation/screen/onboarding/splash_screen.dart';
+import 'package:pr1/presentation/screen/projects/creat_project.dart';
+import 'package:pr1/presentation/screen/projects/show_projects.dart';
 import 'package:pr1/themes/themes.dart';
 
 void main() {
@@ -19,7 +24,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: theme(),
       routes: routes,
-      home: const SplashScreen(),
+      home: const CreatProject(),
     );
   }
 }
