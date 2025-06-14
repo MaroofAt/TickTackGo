96183457205290fee541745bf4bc77adf9141023
unknown <shadishahda23@gmail.com>
2025-06-14 19:00:39 +0300

    just commit

--- flutter/lib/presentation/screen/home/fetching_workspace_failed_popup_menu.dart
+++ flutter/lib/presentation/screen/home/fetching_workspace_failed_popup_menu.dart
@@ -7,6 +7,7 @@ import 'package:pr1/presentation/widgets/gesture_detector.dart';
 import 'package:pr1/presentation/widgets/icons.dart';
 import 'package:pr1/presentation/widgets/text.dart';
 
+// ignore: must_be_immutable
 class FetchingWorkspaceFailedPopupMenu extends StatelessWidget {
   String errorMessage;
 
