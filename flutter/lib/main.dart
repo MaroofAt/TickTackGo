01b08f3c432e629002d03cc3f0408f6c268942e8
unknown <shadishahda23@gmail.com>
2025-06-14 16:15:44 +0300

    merging gharam branch

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pr1/business_logic/workspace_cubit/workspace_cubit.dart';
 import 'package:pr1/core/constance/routes.dart';
-import 'package:pr1/presentation/screen/home/main_home_page.dart';
-import 'package:pr1/presentation/screen/onboarding/onboarding_main.dart';
+import 'package:pr1/presentation/screen/auth/signin.dart';
+import 'package:pr1/presentation/screen/auth/signinnew.dart';
+import 'package:pr1/presentation/screen/auth/signupnew.dart';
 import 'package:pr1/presentation/screen/onboarding/splash_screen.dart';
-import 'package:pr1/presentation/screen/workspace/create_workspace_page.dart';
-import 'package:pr1/presentation/screen/workspace/workspace_info_page.dart';
+import 'package:pr1/presentation/screen/projects/creat_project.dart';
+import 'package:pr1/presentation/screen/projects/show_projects.dart';
 import 'package:pr1/themes/themes.dart';
 
 void main() {
-  // runApp(DevicePreview(builder: (context) => MyApp()));
-  runApp(const MyApp());
+  runApp(DevicePreview(builder: (context) => MyApp()));
+  // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,14 +20,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // builder: DevicePreview.appBuilder,
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       theme: theme(),
       routes: routes,
-      home: BlocProvider(
-        create: (context) => WorkspaceCubit(),
-        child: const MainHomePage(),
-      ),
+      home: const CreatProject(),
     );
   }
 }
