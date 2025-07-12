013acda88c097c7a20c7a88ec23626949cbf1935
unknown <shadishahda23@gmail.com>
2025-07-12 19:59:53 +0300

    token cleared

--- flutter/lib/core/variables/global_var.dart
+++ flutter/lib/core/variables/global_var.dart
@@ -1,4 +1,4 @@
 import '../../data/models/auth/sign_up_model.dart';
 
 SignUpModel? globalSignUpModel;
-String token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyMzM5NzUzLCJpYXQiOjE3NTIzMzYxNTMsImp0aSI6IjBjMDU4NjA4MWY1ODRiMTRhYjBmNjdhYzY3MTY1MzdmIiwidXNlcl9pZCI6MX0.9lrvbxTDuNxE_Jsi7Ua0Hu3f0FzPCFhC_hdm4ZlWY64';
+String token = '';
