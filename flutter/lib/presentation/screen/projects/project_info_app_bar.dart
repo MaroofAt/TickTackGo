b43634267c434e9489a6984865271021ea87b7a3
unknown <shadishahda23@gmail.com>
2025-08-05 16:38:29 +0300

    check admin function | start building project Info page

---
+++ flutter/lib/presentation/screen/projects/project_info_app_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pr1/core/constance/colors.dart';
+import 'package:pr1/presentation/widgets/app_bar.dart';
+import 'package:pr1/presentation/widgets/text.dart';
+
+class ProjectInfoAppBar {
+  static AppBar projectInfoAppBar(BuildContext context) {
+    return MyAppBar.appBar(
+      context,
+      title: MyText.text1('Project Info'),
+      foregroundColor: white,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+    );
+  }
+}
--- flutter/lib/presentation/screen/workspace/workspace_info_page.dart
+++ flutter/lib/presentation/screen/workspace/workspace_info_page.dart
@@ -5,6 +5,7 @@ import 'package:pr1/business_logic/workspace_cubit/workspace_cubit.dart';
 import 'package:pr1/core/constance/constance.dart';
 import 'package:pr1/core/constance/strings.dart';
 import 'package:pr1/core/functions/navigation_functions.dart';
+import 'package:pr1/core/functions/user_functions.dart';
 import 'package:pr1/data/models/workspace/get_workspace_model.dart';
 import 'package:pr1/presentation/screen/invitation/invitation_search.dart';
 import 'package:pr1/presentation/screen/workspace/build_members_list.dart';
@@ -43,79 +44,92 @@ class _WorkspaceInfoPageState extends State<WorkspaceInfoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<WorkspaceCubit, WorkspaceState>(
-        builder: (context, state) {
-          if (state is WorkspaceRetrievingSucceededState) {
-            RetrieveWorkspaceModel retrieveWorkspace = state.retrieveWorkspace;
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  WorkspaceInfoHeader(retrieveWorkspace),
-                  MyDivider.horizontalDivider(thickness: 2, color: Colors.grey),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      MyText.text1(
-                        membersText,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        textColor: Colors.white,
-                      ),
-                      MyGestureDetector.gestureDetector(
-                        onTap: () {
-                          pushScreen(
-                            context,
-                            BlocProvider(
-                              create: (context) => InvitationCubit(),
-                              child: InvitationSearch(
-                                  senderId: 1,
-                                  workspaceId: retrieveWorkspace.id),
+    return SafeArea(
+      child: Scaffold(
+        body: BlocBuilder<WorkspaceCubit, WorkspaceState>(
+          builder: (context, state) {
+            if (state is WorkspaceRetrievingSucceededState) {
+              RetrieveWorkspaceModel retrieveWorkspace = state
+                  .retrieveWorkspace;
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  spacing: 20,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    WorkspaceInfoHeader(retrieveWorkspace),
+                    MyDivider.horizontalDivider(
+                        thickness: 2, color: Colors.grey),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        MyText.text1(
+                          membersText,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          textColor: Colors.white,
+                        ),
+                        MyGestureDetector.gestureDetector(
+                          onTap: () {
+                            pushScreen(
+                              context,
+                              BlocProvider(
+                                create: (context) => InvitationCubit(),
+                                child: InvitationSearch(
+                                    senderId: 1,
+                                    workspaceId: retrieveWorkspace.id),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: width(context) * 0.1,
+                            width: width(context) * 0.3,
+                            decoration: BoxDecoration(
+                              color: Theme
+                                  .of(context)
+                                  .secondaryHeaderColor,
+                              borderRadius: BorderRadius.circular(12),
                             ),
-                          );
-                        },
-                        child: Container(
-                          height: width(context) * 0.1,
-                          width: width(context) * 0.3,
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).secondaryHeaderColor,
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Center(
-                            child: MyText.text1(
-                              'Invite',
-                              textColor:
-                                  Theme.of(context).scaffoldBackgroundColor,
-                              fontSize: 20,
-                              letterSpacing: 2,
-                              fontWeight: FontWeight.w500,
+                            child: Center(
+                              child: MyText.text1(
+                                'Invite',
+                                textColor:
+                                Theme
+                                    .of(context)
+                                    .scaffoldBackgroundColor,
+                                fontSize: 20,
+                                letterSpacing: 2,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: BuildMembersList(
-                      retrieveWorkspace,
+                      ],
                     ),
-                  ),
-                  // Delete Button
-                  // todo compare between user id and owner id
-                  buildDeleteButton(context),
-                ],
-              ),
-            );
-          } else {
-            return Center(
-              child: LoadingIndicator.circularProgressIndicator(
-                color: Theme.of(context).secondaryHeaderColor,
-              ),
-            );
-          }
-        },
+                    Expanded(
+                      child: BuildMembersList(
+                        retrieveWorkspace,
+                      ),
+                    ),
+                    // Delete Button
+                    // todo compare between user id and owner id
+                    // isAdmin(retrieveWorkspace.owner!.) ?
+                        buildDeleteButton(context)
+                        // : Container(),
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                child: LoadingIndicator.circularProgressIndicator(
+                  color: Theme
+                      .of(context)
+                      .secondaryHeaderColor,
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
