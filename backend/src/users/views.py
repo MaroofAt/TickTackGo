7042111a0d45393374fac3a23ccf7a35ed4a958c
ali almsfi <AliAlmusfi>
2025-08-29 21:34:54 +0300

    Try To fi notification first attempt

--- backend/src/users/views.py
+++ backend/src/users/views.py
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import filters
 
-
+import signals
 
 from drf_spectacular.utils import extend_schema
 
@@ -231,6 +231,7 @@ class UserViewSet(viewsets.ModelViewSet):
                     'active': True
                 }
             )
+            signals.send_welcome_notification(User , request.user , True)
             return Response(DeviceSerializer(device).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
 
