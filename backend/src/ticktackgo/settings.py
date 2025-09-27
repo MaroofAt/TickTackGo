d609c07b1ca4965f85c669751501647dad10f06e
MaroofAt <maroof95at@gmail.com>
2025-09-27 10:32:06 +0300

    Fixed ListTasks | Specified Allowed Hosts to settings

--- backend/src/ticktackgo/settings.py
+++ backend/src/ticktackgo/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY' , '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG' , '')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'superformidable-vicente-unvitreous.ngrok-free.dev']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", # REACT + VITE
 ]
